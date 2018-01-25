3f2ba19c9d7955a925e6ff924d90ff02bbc60c32
Max Millington <mmillington@paypal.com>
2018-01-25 08:31:14 -0800

    add axios

--- src/app/home/components/Home.jsx
+++ src/app/home/components/Home.jsx
@@ -1,29 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
 import logo from '../../../logo.svg'
 
-export class Home extends Component {
-  constructor(props) {
-    super(props)
-  }
-  componentDidMount() {
-
-  }
-
-  render() {
-    return (
-      <div className="home">
-        <header className="home-header">
-          <img src={logo} className="home-logo" alt="logo" />
-          <h1 className="home-title">Welcome to React</h1>
-        </header>
-        <p className="home-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        My Sweetness
-      </div>
-    )
-  }
-
+export const Home = (props) => {
+  return (
+    <div className="home">
+      <header className="home-header">
+        <img src={logo} className="home-logo" alt="logo" />
+        <h1 className="home-title">Welcome to React</h1>
+      </header>
+      <p className="home-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+      My Sweetness
+    </div>
+  )
 }
 
 export default Home
